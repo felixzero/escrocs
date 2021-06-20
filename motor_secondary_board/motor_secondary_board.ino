e4f6ef029eeed4107d5578b1dc7740429b97890b
Kevin Guilloy <kevin@guilloy.fr>
2021-06-20 17:28:33 +0200

    Ajout du code de gestion de l'I2C sur la carte moteur esclave

--- motor_secondary_board/motor_secondary_board.ino
+++ motor_secondary_board/motor_secondary_board.ino
@@ -1,24 +1,56 @@
+#include <Wire.h>
+
 #include "motor.h"
 #include "encoder.h"
 
+#define I2C_ADDR 42
+
+#define CMD_READ_ENC 1
+#define CMD_WRITE_SPEED 2
+
 void setup() {
+    Wire.begin(I2C_ADDR);
+    Wire.onReceive(i2cCallback);
+  
     Motor.begin();
     Encoder.begin();
     Serial.begin(9600);
 }
 
 void loop() {
-    goBaby(MotorSingleton::Mode::ClockWise);
-    goBaby(MotorSingleton::Mode::CounterClockWise);
+  // Nothing to loop
+  delay(100);
 }
 
-void goBaby(MotorSingleton::Mode mode) {
-    Motor.writeChannel(0, mode, 250);
-    Motor.writeChannel(1, mode, 250);
-    for (int i = 0; i < 1000; ++i) {
-            long encoder = Encoder.readChannel(1);
-            Serial.print("Encoder read: ");
-            Serial.println(encoder);
-            delay(10);
+void i2cCallback(int howMany) {
+    while (howMany >= 2) {
+      manageI2cCommand(Wire.read(), Wire.read());
+      howMany -= 2;
     }
+}
+
+void manageI2cCommand(uint8_t command, uint8_t argument) {
+  uint8_t queryChannel = command & 0x3;
+  uint8_t queryCommand = command >> 2;
+
+  switch (queryCommand) {
+    case CMD_READ_ENC:
+      writeLong(Encoder.readChannel(queryChannel));
+      break;
+
+    case CMD_WRITE_SPEED:
+      Motor.writeSpeed(queryChannel, argument);
+      writeLong(Encoder.readChannel(queryChannel));
+      break;
+
+    default:
+      writeLong(0xFFFFFFFF);
+  }
+}
+
+void writeLong(long value) {
+  for (int i = 0; i < 4; ++i) {
+    Wire.write((char)value);
+    value >>= 8;
+  }
 }
