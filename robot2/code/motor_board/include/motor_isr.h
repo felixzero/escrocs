98dac8c9345ed3d0d44af7aee470eda23fe7a9b0
Kevin Guilloy <kevin@guilloy.fr>
2021-09-08 22:13:02 +0200

    Motor PWM control with double buffering

--- robot2/code/motor_board/include/motor_isr.h
+++ robot2/code/motor_board/include/motor_isr.h
@@ -2,7 +2,16 @@
 
 #include <Arduino.h>
 
-extern "C" volatile uint8_t portb_values[4];
-extern "C" volatile uint8_t portd_values[4];
-extern "C" volatile uint8_t channel_values[4];
-extern "C" volatile uint8_t current_index;
+extern "C" struct motor_pwm_data {
+    uint8_t portb_values[4];
+    uint8_t portd_values[4];
+    uint8_t channel_values[4];
+} __attribute__((packed));
+
+extern "C" volatile struct motor_pwm_data motor_pwm_buffer_1;
+extern "C" volatile struct motor_pwm_data motor_pwm_buffer_2;
+extern "C" volatile struct motor_pwm_data *current_pwm_buffer;
+extern "C" volatile uint8_t pwm_buffer_swap;
+
+#define idle_pwm_buffer() (current_pwm_buffer == &motor_pwm_buffer_1 ? &motor_pwm_buffer_2 : &motor_pwm_buffer_1)
+#define request_pwm_buffer_swap() (pwm_buffer_swap = 1)
--- robot2/code/motor_board/src/motor.cpp
+++ robot2/code/motor_board/src/motor.cpp
@@ -3,20 +3,22 @@
 #include "motor.h"
 #include "motor_isr.h"
 
-#define _C1A_REG portd_values
+#define _C1A_REG new_buffer->portd_values
 #define _C1A_FLAG 0b00100000u
-#define _C1B_REG portd_values
+#define _C1B_REG new_buffer->portd_values
 #define _C1B_FLAG 0b01000000u
 
-#define _C2A_REG portb_values
+#define _C2A_REG new_buffer->portb_values
 #define _C2A_FLAG 0b00000010u
-#define _C2B_REG portb_values
+#define _C2B_REG new_buffer->portb_values
 #define _C2B_FLAG 0b00000100u
 
-#define _C3A_REG portd_values
+#define _C3A_REG new_buffer->portd_values
 #define _C3A_FLAG 0b00001000u
-#define _C3B_REG portb_values
+#define _C3B_REG new_buffer->portb_values
 #define _C3B_FLAG 0b00000001u
+
+#define MAX_ALLOWED_VALUE 253
 
 void init_motors()
 {
@@ -26,8 +28,9 @@ void init_motors()
     TCCR1A = 0;
     TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS11) | _BV(CS10);
     // Enable COMPA interrupt
-    TIMSK1 = _BV(OCIE1A);
+    TIMSK1 = _BV(OCIE1A) | _BV(OCIE1B);
     ICR1 = 255;
+    OCR1B = 0;
 
     interrupts();
 
@@ -41,10 +44,12 @@ void init_motors()
 
 void write_motor_speed(float speed1, float speed2, float speed3)
 {
+    volatile struct motor_pwm_data *new_buffer = idle_pwm_buffer();
+
     uint8_t timings[3];
-    timings[0] = (uint8_t)(255 - min(1.0, fabs(speed1)) * 254);
-    timings[1] = (uint8_t)(255 - min(1.0, fabs(speed2)) * 254);
-    timings[2] = (uint8_t)(255 - min(1.0, fabs(speed3)) * 254);
+    timings[0] = (uint8_t)(255 - min(1.0, fabs(speed1)) * MAX_ALLOWED_VALUE);
+    timings[1] = (uint8_t)(255 - min(1.0, fabs(speed2)) * MAX_ALLOWED_VALUE);
+    timings[2] = (uint8_t)(255 - min(1.0, fabs(speed3)) * MAX_ALLOWED_VALUE);
 
     volatile uint8_t *regs[3];
     regs[0] = (speed1 > 0.0) ? _C1A_REG : _C1B_REG;
@@ -57,22 +62,43 @@ void write_motor_speed(float speed1, float speed2, float speed3)
     flags[2] = (speed3 > 0.0) ? _C3A_FLAG : _C3B_FLAG;
 
     // Values at t=0: everything is high
-    channel_values[0] = 0;
+    new_buffer->channel_values[0] = 0;
     for (int i = 0; i < 4; ++i) {
-        portb_values[i] = 0xFF;
-        portd_values[i] = 0xFF;
+        new_buffer->portb_values[i] = 0xFF;
+        new_buffer->portd_values[i] = 0xFF;
     }
 
-    for (uint8_t j = 1; j < 4; ++j) {
+    // Compute interrupt times and register values at each time
+    uint8_t managed_channels = 0;
+    uint8_t set_timings = 0;
+    while (managed_channels < 3) {
         uint8_t min_timing = min(timings[0], min(timings[1], timings[2]));
-        channel_values[j] = min_timing;
+        new_buffer->channel_values[set_timings + 1] = min_timing;
+
         for (uint8_t i = 0; i < 3; ++i) {
             if (timings[i] == min_timing) {
-                for (uint8_t k = j; k < 4; ++k) {
+                for (uint8_t k = set_timings + 1; k < 4; ++k) {
                     regs[i][k] &= ~flags[i];
                 }
                 timings[i] = 255;
+                managed_channels++;
             }
         }
+
+        set_timings++;
     }
+
+    // Insert dummy values to manage duplicates
+    for (uint8_t i = 3; i > 3 - set_timings; --i) {
+        new_buffer->channel_values[i] = new_buffer->channel_values[i - 3 + set_timings];
+        new_buffer->portb_values[i] = new_buffer->portb_values[i - 3 + set_timings];
+        new_buffer->portd_values[i] = new_buffer->portd_values[i - 3 + set_timings];
+    }
+    for (uint8_t i = 1; i < 4 - set_timings; ++i) {
+        new_buffer->channel_values[i] = i;
+        new_buffer->portb_values[i] = new_buffer->portb_values[0];
+        new_buffer->portd_values[i] = new_buffer->portd_values[0];
+    }
+
+    request_pwm_buffer_swap();
 }
