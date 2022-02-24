a049f05e06da97f1b3befe51caa4619a76a5bdf1
Kevin Guilloy <kevin@guilloy.fr>
2022-02-24 22:23:59 +0100

    Update fuse settings

--- board_config/atmega168p/write_bootloader.sh
+++ board_config/atmega168p/write_bootloader.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-avrdude -c stk500v1 -p m168p -P /dev/ttyUSB0 -b 19200 -U lfuse:w:0xFF:m -U hfuse:w:0xDE:m -U efuse:w:0xFD:m -U flash:w:optiboot_atmega168.hex:i
+avrdude -c usbasp -p m168p -U lfuse:w:0xFF:m -U hfuse:w:0xDE:m -U efuse:w:0xFD:m -U flash:w:optiboot_atmega168.hex:i
 
