24ab79352178b8b5a245dbfcda3868e175cf56ba
MRNL- <martin.raynal@free.fr>
2023-05-18 05:44:02 +0200

    Terrain model for 2023

--- misc/HMI/PyCommandOTA.py
+++ misc/HMI/PyCommandOTA.py
@@ -2,13 +2,25 @@ import requests
 import tkinter.messagebox
 from tkinter import *
 from tkinter import ttk
+from tkinter import scrolledtext
 import numpy as np
 import platform
 import subprocess
+import socket
+import threading
+
 
 #headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
 DEFAULT_HOST = "192.168.4.1"
+DEFAULT_HOST_SQUARE = "192.168.5.1"
+DEFAULT_HOST_TRIANGLE = "192.168.6.1"
+
+DEFAULT_HOST = DEFAULT_HOST_TRIANGLE
+
+CAKE_FLOOR_LAYER = 2200
+CAKE_MID_LAYER = 1550
+CAKE_TOP_LAYER = 800
 
 def ping(host):
     param = '-n' if platform.system().lower()=='windows' else '-c'
@@ -59,7 +71,13 @@ def set_Pose():
         can1.itemconfig(ping_oval, fill = "red")
         tkinter.messagebox.showerror("Error","ESCROBOT @"+ selectedHost.get() + " is offline.")
         return;
-    r = requests.post("http://" + selectedHost.get() + "/" + 'action/set_pose', data = '{"x": ' + write_pos_X.get() + ', "y": ' + write_pos_Y.get() + ', "theta": ' + str(np.radians(float(write_pos_T.get()))) + ', "perform_detection": false}')
+
+    if performDetect.get() == 0:
+        det="false"
+    else:
+        det = "true"
+    
+    r = requests.post("http://" + selectedHost.get() + "/" + 'action/set_pose', data = '{"x": ' + write_pos_X.get() + ', "y": ' + write_pos_Y.get() + ', "theta": ' + str(np.radians(float(write_pos_T.get()))) + ', "perform_detection": ' + det +'}')
     print("Set_pose")
     print(r)
     print(r.text)
@@ -74,6 +92,40 @@ def move_stepper():
     print(r)
     print(r.text)
 
+
+def lower_pump_to_floor():
+    if not ping(selectedHost.get()):
+        can1.itemconfig(ping_oval, fill = "red")
+        tkinter.messagebox.showerror("Error","ESCROBOT @"+ selectedHost.get() + " is offline.")
+        return;
+    channel = channel_selected.get()
+    if channel == 1:
+        move_stepper(channel, CAKE_FLOOR_LAYER, 0.2)
+    else:
+        move_stepper(channel, -CAKE_FLOOR_LAYER, 0.2)
+
+def lower_pump_to_mid():
+    if not ping(selectedHost.get()):
+        can1.itemconfig(ping_oval, fill = "red")
+        tkinter.messagebox.showerror("Error","ESCROBOT @"+ selectedHost.get() + " is offline.")
+        return;
+    channel = channel_selected.get()
+    if channel == 1:
+        move_stepper(channel, CAKE_MID_LAYER, 0.2)
+    else:
+        move_stepper(channel, -CAKE_MID_LAYER, 0.2)
+        
+def lower_pump_to_top():
+    if not ping(selectedHost.get()):
+        can1.itemconfig(ping_oval, fill = "red")
+        tkinter.messagebox.showerror("Error","ESCROBOT @"+ selectedHost.get() + " is offline.")
+        return;
+    channel = channel_selected.get()
+    if channel == 1:
+        move_stepper(channel, CAKE_TOP_LAYER, 0.2)
+    else:
+        move_stepper(channel, -CAKE_TOP_LAYER, 0.2)
+
 def check_liveliness():
     if not ping(selectedHost.get()):
         can1.itemconfig(ping_oval, fill = "red")
@@ -82,14 +134,31 @@ def check_liveliness():
         can1.itemconfig(ping_oval, fill = "green")
 
 
+def UDP_client_thread(name):   
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.bind(("", 11111))
+    while True:
+        print("reading:")
+        data, addr = sock.recvfrom(1024)
+        print(data)
+        textExample.insert("end", "\n"+ "MAIN: " + str(data))
 
+
+def UDP_client_LIDAR_thread(name):       
+    sockL = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sockL.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sockL.bind(("", 11112))
+    while True:
+        print("reading:")
+        data, addr = sockL.recvfrom(1024)
+        print(data)      
+        textExample.insert("end", "\n"+ "LIDAR: " + str(data))
 
 
 connected = ping(DEFAULT_HOST)
 if connected:
     print("Escrobot is online.")
-    #r2 = requests.post('http://192.168.4.1/action/move_stepper', data = '{"channel" : 0, "target": 0, "speed": 0.1}')
-    #print(r2)
 else:
     print("Escrobot is offline.")
 
@@ -186,7 +255,11 @@ btnO=Button(posPane,text="Overwrite", command=lambda: overwrite_Pose())
 btnO.grid(row=4, column=0, columnspan=3, sticky=W+E);
 
 btnS=Button(posPane,text="Send", command=lambda: set_Pose())
-btnS.grid(row=5, column=0, columnspan=3, sticky=W+E);
+btnS.grid(row=5, column=0, columnspan=2, sticky=W+E);
+
+performDetect=IntVar();
+checkDetect=Checkbutton(posPane, text = "Detection", variable = performDetect, onvalue = 1,offvalue = 0)
+checkDetect.grid(row=5, column=2, columnspan=1, sticky=W+E);
 
 posPane.grid(row=1,column=0, sticky=W+E)
 #--------------------------------------------------------
@@ -209,6 +282,8 @@ lblSp.grid(row=2,column=0)
 channel_selected = tkinter.StringVar()
 chcombobox = ttk.Combobox(stepPane, textvariable=channel_selected)
 chcombobox['values'] = ('0', '1', '2', '3')
+channel_selected.set('0')
+
 chcombobox.grid(row=0, column=1)
 
 target_selected = tkinter.StringVar()
@@ -224,13 +299,61 @@ entrySpeed.grid(row=2, column=1)
 btnM=Button(stepPane,text="Move stepper", command=lambda: move_stepper())
 btnM.grid(row=3, column=0, columnspan=2, sticky=W+E);
 
+btnMtf=Button(stepPane,text="Move stepper to floor", command=lambda: lower_pump_to_floor())
+btnMtf.grid(row=4, column=0, columnspan=1, sticky=W+E);
+btnMtm=Button(stepPane,text="Move stepper to mid", command=lambda: lower_pump_to_mid())
+btnMtm.grid(row=4, column=1, columnspan=1, sticky=W+E);
+btnMtt=Button(stepPane,text="Move stepper to top", command=lambda: lower_pump_to_mid())
+btnMtt.grid(row=4, column=2, columnspan=1, sticky=W+E);
+
 stepPane.grid(row=1, column=1, sticky=W+E)
 #--------------------------------------------------------
+
+#------ Pump controls -----------------------------------
+##pumpPane = ttk.Frame(win, relief=GROOVE)
+##pumpPane['padding'] = (10,5,10,5)
+##pumpPane.rowconfigure(0, pad=10)
+##pumpPane.rowconfigure(1, pad=10)
+##pumpPane.rowconfigure(2, pad=10)
+##
+##lblCh = Label(pumpPane, text="Channel: ")
+##lblCh.grid(row=0,column=0)
+##lblTa = Label(pumpPane, text="State: ")
+##lblTa.grid(row=1,column=0)
+##
+##channel_selected = tkinter.StringVar()
+##chcombobox = ttk.Combobox(pumpPane, textvariable=channel_selected)
+##chcombobox['values'] = ('0', '1', '2', '3')
+##channel_selected.set('0')
+##
+##chcombobox.grid(row=0, column=1)
+##
+##target_selected = tkinter.StringVar()
+##target_selected.set('0')
+##entryTarget = Entry(pumpPane, textvariable=target_selected)
+##entryTarget.grid(row=1, column=1)
+##
+##speed_selected = tkinter.StringVar()
+##speed_selected.set('0.15')
+##entrySpeed = Entry(pumpPane, textvariable=speed_selected)
+##entrySpeed.grid(row=2, column=1)
+##
+###btnM=Button(pumpPane,text="Move stepper", command=lambda: move_stepper())
+###btnM.grid(row=3, column=0, columnspan=2, sticky=W+E);
+##
+##pumpPane.grid(row=2, column=1, sticky=W+E)
+#--------------------------------------------------------
+
+textExample = scrolledtext.ScrolledText(win, wrap = WORD)
+textExample.grid(row=2, column=0, sticky=N+S+W+E)
 
 terrain=Canvas(win, bg='white')
 terrain.grid(row=3,column=3);
 
-
-
+t = threading.Thread(target=UDP_client_thread, args=(1,))
+t.start()
+tL = threading.Thread(target=UDP_client_LIDAR_thread, args=(1,))
+tL.start()
 
 win.mainloop()
+
