8de771136c513cd51aca1b7db0155912e1cf1b30
Kévin Guilloy <kevin@guilloy.fr>
2023-07-21 21:59:35 +0200

    Updated version of motor board and main board (#3)
    * Cleaning backup files
    
    * [WIP] Main board V2
    
    * [WIP] Implement RS485 in ultrasonic board
    
    * Updated version of motor board and main board

--- code/ultrasonic_board/src/main.c
+++ code/ultrasonic_board/src/main.c
@@ -2,6 +2,10 @@
 #include "controller.h"
 #include "led_strip.h"
 #include "ultrasound.h"
+#include "modbus.h"
+
+#include <avr/io.h>
+#include <avr/delay.h>
 
 int main()
 {
@@ -13,4 +17,29 @@ int main()
     while (true) {
         perform_ultrasound_scan(poll_i2c_operations);
     }
+
+    //init_modbus();
+/*
+#if 0
+    while (true) {
+        send_character('H');
+        send_character('e');
+        send_character('l');
+        send_character('l');
+        send_character('o');
+        send_character('!');
+        send_character('\n');
+    }
+#else
+    DDRB |= _BV(1);
+    PORTB &= ~_BV(2);
+    while (true) {
+        if (poll_character() == 'l') {
+            PORTB |= _BV(1);
+        } else {
+            PORTB &= ~_BV(1);
+        }
+    }
+*/
+#endif
 }
---
+++ code/ultrasonic_board/src/modbus.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void init_modbus(void);
+void send_character(char c);
+char poll_character(void);
