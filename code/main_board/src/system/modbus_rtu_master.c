e4f1b0de6af9a4c7a25b8157906fc5af848024ff
Kévin Guilloy <kevin@guilloy.fr>
2024-05-06 19:56:21 +0200

    Lastminute (#13)
    * Make obstacle avoidance refuse obstacles outside of the terrain
    
    * Motor board back to RS485 (but with I2C on us)

--- code/main_board/src/system/modbus_rtu_master.c
+++ code/main_board/src/system/modbus_rtu_master.c
@@ -5,6 +5,8 @@
 
 #include <freertos/FreeRTOS.h>
 #include <string.h>
+#include <driver/uart.h>
+#include <hal/uart_hal.h>
 #include <esp_err.h>
 #include <esp_log.h>
 
@@ -41,16 +43,73 @@ static esp_err_t read_registers(
 static esp_err_t query_modbus_rtu(size_t message_len, size_t expected_len, uint8_t slave_addr, uint8_t function_code, uint8_t *query_buffer, uint8_t *response_buffer);
 static void send_raw_i2c(const void *buffer, size_t len);
 static size_t receive_raw_i2c(void *buffer, size_t expected_len);
+static void send_raw_rs485(const void *buffer, size_t len);
+static size_t receive_raw_rs485(void *buffer, size_t expected_len);
 
 #define QUERY_BUFFER_DATA (&query_buffer[2])
 #define RESPONSE_BUFFER_DATA (&response_buffer[2])
 
 static SemaphoreHandle_t modbus_mutex;
 static uint8_t current_i2c_address_for_modbus = 0;
+static uart_hal_context_t hal_context;
+static spinlock_t spinlock = portMUX_INITIALIZER_UNLOCKED;
+
+static void IRAM_ATTR uart_intr_handle(void *arg);
 
 esp_err_t init_modbus_rtu_master(void)
 {
+    esp_err_t err = ESP_OK;
     ESP_LOGI(TAG, "Initializing Modbus subsystem");
+
+    // Configure UART to RS485 transceiver
+    err = uart_driver_install(UART_NUM, 2 * BUFFER_SIZE, 0, 0, NULL, 0);
+    if (err) {
+        ESP_ERROR_CHECK_WITHOUT_ABORT(err);
+        return err;
+    }
+
+    uart_config_t uart_config = {
+        .baud_rate = UART_BAUD_RATE,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_2,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+    };
+    err = uart_param_config(UART_NUM, &uart_config);
+    if (err) {
+        ESP_ERROR_CHECK_WITHOUT_ABORT(err);
+        return err;
+    }
+
+    err = uart_set_pin(UART_NUM, TX_PIN, RX_PIN, TXE_PIN, UART_PIN_NO_CHANGE);
+    if (err) {
+        ESP_ERROR_CHECK_WITHOUT_ABORT(err);
+        return err;
+    }
+
+    // Unregister software UART ISR and register ours
+    hal_context.dev = UART_LL_GET_HW(UART_NUM);
+
+    err = uart_isr_free(UART_NUM);
+    if (err) {
+        ESP_ERROR_CHECK_WITHOUT_ABORT(err);
+        return err;
+    }
+
+    err = uart_isr_register(UART_NUM, uart_intr_handle, NULL, ESP_INTR_FLAG_IRAM, NULL);
+    if (err) {
+        ESP_ERROR_CHECK_WITHOUT_ABORT(err);
+        return err;
+    }
+
+    uart_intr_config_t intr_config = {
+        .intr_enable_mask = UART_TX_DONE_INT_ENA_M
+    };
+    err = uart_intr_config(UART_NUM, &intr_config);
+    if (err) {
+        ESP_ERROR_CHECK_WITHOUT_ABORT(err);
+        return err;
+    }
 
     modbus_mutex = xSemaphoreCreateMutex();
     if (!modbus_mutex) {
@@ -244,11 +303,17 @@ static esp_err_t query_modbus_rtu(size_t message_len, size_t expected_len, uint8
     query_buffer[message_len + 2] = crc >> 8;
     query_buffer[message_len + 3] = crc & 0xFF;
 
-    xSemaphoreTake(modbus_mutex, portMAX_DELAY);
-    current_i2c_address_for_modbus = slave_addr == 0x44 ? 0x11 : 0x12; // FIXME: Ugly hack, fix the API!!!
-    send_raw_i2c(query_buffer, message_len + 4);
-    size_t received_len = receive_raw_i2c(response_buffer, expected_len);
-    xSemaphoreGive(modbus_mutex);
+    // FIXME: Ugly hack
+    size_t received_len;
+    if (slave_addr == 0x44) { // if motor
+        send_raw_rs485(query_buffer, message_len + 4);
+        received_len = receive_raw_rs485(response_buffer, expected_len);
+    } else { // us
+        current_i2c_address_for_modbus = 0x12;
+        send_raw_i2c(query_buffer, message_len + 4);
+        received_len = receive_raw_i2c(response_buffer, expected_len);
+    }
+
     if (received_len <= 0) {
         return ESP_ERR_TIMEOUT;
     }
@@ -277,4 +342,47 @@ static size_t receive_raw_i2c(void *buffer, size_t expected_len)
 {
     request_from_i2c(I2C_PORT_MOTOR, current_i2c_address_for_modbus, buffer, expected_len);
     return expected_len;
+}
+
+static void send_raw_rs485(const void *buffer, size_t len)
+{
+    uint32_t write_size = 0;
+
+    uart_hal_rxfifo_rst(&hal_context);
+    uart_hal_set_rts(&hal_context, 0);
+    uart_hal_write_txfifo(&hal_context, buffer, len, &write_size);
+}
+
+static size_t receive_raw_rs485(void *buffer, size_t expected_len)
+{
+    int rx_buffer_length = 0, length = 0;
+    int64_t start_listening_time = esp_timer_get_time();
+
+    while ((rx_buffer_length == 0) || (length != 0)) {
+        length = 0;
+        uart_hal_read_rxfifo(&hal_context, buffer + rx_buffer_length, &length);
+        rx_buffer_length += length;
+        int64_t initial_wait_time = esp_timer_get_time();
+
+        if (esp_timer_get_time() > start_listening_time + RECEIVE_TIMEOUT_US) {
+            break;
+        }
+        while (esp_timer_get_time() <= initial_wait_time + ECHO_READ_TOUT * 11);
+    }
+
+    uart_hal_rxfifo_rst(&hal_context);
+    return rx_buffer_length;
+}
+
+static void IRAM_ATTR uart_intr_handle(void *arg)
+{
+    uint32_t uart_intr_status = uart_hal_get_intsts_mask(&hal_context);
+
+    if (uart_intr_status & UART_INTR_TX_DONE) {
+        portENTER_CRITICAL_ISR(&spinlock);
+        uart_hal_set_rts(&hal_context, 1);
+        uart_hal_clr_intsts_mask(&hal_context, UART_INTR_TX_DONE);
+        portEXIT_CRITICAL_ISR(&spinlock);
+        return;
+    }
 }
